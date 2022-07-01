c40634593f95a9efc1aaa93f92e07001c074691b
Maipa <Maipa6686@gmail.com>
2022-07-01 19:23:01 -0300

    se agrego login y authentication

--- entrega/urls.py
+++ entrega/urls.py
@@ -1,21 +1,6 @@
-"""entrega URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path, include
-from entrega.views import index, contacto, search
+from entrega.views import index, contacto, search, login_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -24,5 +9,6 @@ urlpatterns = [
     path('alumnos/', include ('alumnos.urls')),
     path('docentes/', include ('docentes.urls')),
     path('contacto/', contacto, name = 'contacto'),
-    path('search/', search, name = 'search')
+    path('search/', search, name = 'search'),
+    path('login/', login_view, name = 'login'),
 ]
--- entrega/views.py
+++ entrega/views.py
@@ -1,14 +1,48 @@
 from datetime import datetime
 from genericpath import exists
-
+from multiprocessing import context
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from carreras.models import Contacto
 from alumnos.models import Alumno
 from carreras.models import Carrera
 from docentes.models import Docente
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import authenticate, login
+
+
+def login_view(request):
+
+        if request.method == 'POST':
+                form = AuthenticationForm(request, data = request.POST)
+
+                if form.is_valid():
+                        username = form.cleaned_data['username']
+                        password = form.cleaned_data['password']
+                        user = authenticate(username=username, password=password)
+
+                        if user is not None:
+                                login(request, user)
+                                context = {'message':f'Bienvenido {username}'}
+                                return render(request,'index.html', context=context)
+                        else:
+                                context = {'errors':'no existe ningun usuario con esas credenciales'}
+                                form = AuthenticationForm()
+                                return render(request, 'auth/login.html', context = context)
+                else:
+                        errors = form.errors
+                        form=AuthenticationForm()
+                        context = {'errors':errors, 'form':form}
+                        return render(request, 'auth/login.html', context = context)
+
+        else:
+                form = AuthenticationForm()
+                context = {'form':form}
+                return render(request, 'auth/login.html', context = context)
 
 def index(request):
+        print(request.user)
+        print(request.user.is_authenticated)
         return render(request, 'index.html')
 
 def contacto(request):
